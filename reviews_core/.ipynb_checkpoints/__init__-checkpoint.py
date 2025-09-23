8dd2b69f03fa265f234b8883881f9d3eaf1b326b
pedrocatarino-novaims <20241139@novaims.unl.pt>
2025-09-23 12:30:43 +0100

    update init.py

--- reviews_core/.ipynb_checkpoints/__init__-checkpoint.py
+++ reviews_core/.ipynb_checkpoints/__init__-checkpoint.py
@@ -1,7 +1,7 @@
 #from .cleaning import cleaning
 #from .topic_modeling import apply_ldas
 #from .vader_sentiment import add_vader_sentiment
-#from .get_sample import get_sample
+from .get_sample import get_sample
 
 __all__ = [
     #"cleaning",
